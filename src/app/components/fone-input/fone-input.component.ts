f058adee4f2222e18c870015d482d361122268a9
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2024-02-28 06:54:11 -0300

    Form solicitacao

--- src/app/components/fone-input/fone-input.component.ts
+++ src/app/components/fone-input/fone-input.component.ts
@@ -1,18 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fone-input',
-  standalone: true,
-  imports: [],
-  templateUrl: './fone-input.component.html',
-  styleUrl: './fone-input.component.css'
+	selector: 'app-fone-input',
+	standalone: true,
+	imports: [],
+	templateUrl: './fone-input.component.html',
+	styleUrl: './fone-input.component.css',
 })
 export class FoneInputComponent implements OnInit {
-
-	@Input() fala!:string;
+	@Input() text!: string;
 
 	ngOnInit(): void {
-		console.log(this.fala);
+		let fone = document.getElementById('fone') as HTMLInputElement | null;
+		if (fone != null) {
+			if (fone.value.length == 0) {
+				fone.value += '(';
+			}
+		}
 	}
 
+	sendit() {
+		let fone = document.getElementById('fone') as HTMLInputElement | null;
+		if (fone != null) {
+			if (fone.value.length == 0) {
+				fone.value += '(';
+			}
+
+			if (fone.value.length == 1) {
+				fone.value += ') ';
+			}
+		}
+	}
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,40 +2,83 @@ import { Component } from '@angular/core';
 
 import { Applicant } from '../../models/Applicant';
 import { CommonModule } from '@angular/common';
+
 import { FoneInputComponent } from '../fone-input/fone-input.component';
+import {
+	FormBuilder,
+	FormGroup,
+	FormsModule,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
+
+import Cursos from '../../../assets/dataCursos.json';
+import Servicos from '../../../assets/dataServicos.json';
 
 @Component({
 	selector: 'app-main',
 	standalone: true,
-	imports: [CommonModule,FoneInputComponent],
 	templateUrl: './main.component.html',
 	styleUrl: './main.component.css',
+	imports: [
+		CommonModule,
+		FoneInputComponent,
+		ReactiveFormsModule,
+		FormsModule,
+	],
 })
 export class MainComponent {
 	//public a: Applicant = new Applicant();
 
-	label:String = "fone";
+	public formProtocolo: FormGroup;
 
-	public cursos = [
-		{
-			id: 1,
-			nome: 'Ciência da Computação',
-		},
-		{
-			id: 2,
-			nome: 'Engenharia Mecânica',
-		},
-		{
-			id: 3,
-			nome: 'Medicina',
-		},
-		{
-			id: 4,
-			nome: 'Direito',
-		},
-		{
-			id: 5,
-			nome: 'Administração de Empresas',
-		},
-	];
+	private CursosJson = Cursos;
+	public servicos = Servicos;
+	public cursos: { id: number; nome: string; modalidade: string }[] = [];
+	public turno = ['Matutino', 'Noturno'];
+	public modalidade = ['PRESENCIAL', 'EAD', 'HIBRIDO'];
+
+	public isMulti = false;
+
+	constructor(private fb: FormBuilder) {
+		this.formProtocolo = this.fb.group({
+			modalidade: ['', Validators.compose([Validators.required])],
+			ra: ['', Validators.compose([Validators.required])],
+			nome: ['', Validators.compose([Validators.required])],
+			curso: ['', Validators.compose([Validators.required])],
+			servicoSelecionado: ['', Validators.compose([Validators.required])],
+		});
+	}
+
+	newProtocolo() {
+		console.log('Novo protocolo');
+	}
+
+	loadCursos() {
+		var data = this.formProtocolo.value;
+		if (this.cursos.length != 0) {
+			this.cursos = [];
+		}
+		this.getListCursos(data.modalidade);
+	}
+
+	isMultiOrNot() {
+		let data = this.formProtocolo.value;
+		console.log(data);
+
+		if (data.servicoSelecionado.multi == true) {
+			this.isMulti = true;
+		}
+		if (data.servicoSelecionado.multi == false) {
+			this.isMulti = false;
+		}
+	}
+
+	getListCursos(modalidade: String) {
+		this.CursosJson.forEach((e) => {
+			if (e.modalidade == modalidade) {
+				this.cursos.push(e);
+			}
+		});
+	}
 }
